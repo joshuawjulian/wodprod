db6caa6d0f7b759543ce985055c7f8d903179daf
Joshua Julian <joshuawjulian@gmail.com>
2025-10-31 17:16:12 +0000

    Going with typed errors, not thrown

---
+++ src/lib/server/auth/utils.ts
@@ -0,0 +1,31 @@
+import argon2 from 'argon2';
+import { Err, Ok, type Result } from 'ts-results';
+
+export async function hashPassword(
+	password: string
+): Promise<Result<string, Error>> {
+	try {
+		// Hash the password using Argon2
+		return Ok(await argon2.hash(password));
+	} catch (error) {
+		console.error('Verification failed:', error);
+		if (error instanceof Error) {
+			return Err(error);
+		}
+		return Err(new Error('An unknown error occurred'));
+	}
+}
+export async function verifyPassword(
+	hashedPassword: string,
+	password: string
+): Promise<Result<boolean, Error>> {
+	try {
+		return Ok(await argon2.verify(hashedPassword, password));
+	} catch (error) {
+		console.error('Verification failed:', error);
+		if (error instanceof Error) {
+			return Err(error);
+		}
+		return Err(new Error('An unknown error occurred'));
+	}
+}
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,5 +2,5 @@ import postgres from 'postgres';
 
 export const sql = postgres(Bun.env.POSTGRES_URL || '', {
 	onnotice: () => false,
-	max: 10, // Single connection for migration script
+	max: 10
 });
